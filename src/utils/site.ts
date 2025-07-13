69d3647874759cb1b6a79a6f962196f0b9a73fe7
eslasojica <keslasojica@gmail.com>
2025-07-14 06:50:16 +0700

    Delete src directory

--- src/utils/site.ts
+++
@@ -1,4 +0,0 @@
-export const nama = 'Bokep Smp Pornhub';
-export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Smp Mama Jepang Viral Sma Cindo Tante China';
-export const url = 'https://bokepsmppornub.pages.dev';
-export const terbit = '2025-06-25T00:01:03Z';
